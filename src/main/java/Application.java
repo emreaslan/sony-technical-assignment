d17e324ee14d9db9db9893bdb77b1adc7693e1d8
Emre Aslan <emre55aslan@gmail.com>
2019-09-20 13:22:50 +0300

    Batch processing

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -10,33 +10,32 @@ import static java.lang.System.in;
 
 public class Application {
 
-    public static void main(String[] args) {
+    public static List getInput() {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in));
+        List<Map<Integer, Node>> problems = new ArrayList<>();
 
         try {
             int numberOfProblems = Integer.parseInt(bufferedReader.readLine());
-            while (numberOfProblems > 0){
+            while (numberOfProblems > 0) {
+
                 int numberOfCities = Integer.parseInt(bufferedReader.readLine());
-                Map<Integer, Node> citiesMap = new HashMap<>();
-                for(int i=0; i<numberOfCities; ++i){
-                    citiesMap.put(i, new Node(i));
-                }
-
                 int[] roads = Arrays.stream(bufferedReader.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-                for(int i=0; i<numberOfCities-1; ++i){
-                    citiesMap.get(i+1).addNeighbour(citiesMap.get(roads[i]));
-                    citiesMap.get(roads[i]).addNeighbour(citiesMap.get(i+1));
-                }
 
-                citiesMap.forEach( (k,v) -> System.out.println(v));
+                Map<Integer, Node> citiesMap = new HashMap<>();
+                if (numberOfCities == roads.length + 1) {
+                    for (int i = 0; i < numberOfCities; ++i) {
+                        citiesMap.put(i, new Node(i));
+                    }
+
+                    for (int i = 0; i < numberOfCities - 1; ++i) {
+                        citiesMap.get(i + 1).addNeighbour(citiesMap.get(roads[i]));
+                        citiesMap.get(roads[i]).addNeighbour(citiesMap.get(i + 1));
+                    }
+                }
 
                 //solution
-                Comparator<Node> comparator = Comparator.comparing(Node::getDegree);
-                Node nodeMaxDegree = citiesMap.values().stream().max(comparator).get();
-
-                citiesMap.forEach( (k,v) -> System.out.println(v));
-
-                System.out.println( "Output: " + getResult(citiesMap));
+                problems.add(citiesMap);
+                //System.out.println( "Output: " + getResult(numberOfCities, citiesMap));
 
                 // end of solution
                 --numberOfProblems;
@@ -44,29 +43,45 @@ public class Application {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return problems;
     }
 
-    private static int getResult(Map<Integer, Node> citiesMap) {
+    public static void solveProblems(List<Map<Integer, Node>> problemList) {
+        problemList.forEach(problem -> {
+            try {
+                System.out.println("Output: " + getResult(problem));
+            } catch (Exception e) {
+                System.out.println("Output: " + e.getMessage());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        solveProblems(getInput());
+    }
+
+    private static int getResult(Map<Integer, Node> citiesMap) throws Exception {
+        if (citiesMap.isEmpty()) {
+            throw new Exception("Input Missmatch Error!");
+        }
         int step = 0;
         Set<Node> toBeConnected = new HashSet<>();
         Map<Integer, Integer> mergeMap = new HashMap<>();
 
-        System.out.println("First CitiesMap= " + citiesMap);
+        //System.out.println("First CitiesMap= " + citiesMap);
 
-        while (citiesMap.keySet().size() > 1){
-        //for(int tt=0; tt<4; ++tt){
+        while (citiesMap.keySet().size() > 1) {
             Map<Integer, Set<Integer>> degreeMap = citiesMap.values().stream()
                     .collect(Collectors.groupingBy(Node::getDegree,
                             Collectors.mapping(Node::getId, Collectors.toSet())));
 
-            System.out.println("DegreeMap= " + degreeMap);
-            int degreeCount = degreeMap.keySet().size();
+            //System.out.println("DegreeMap= " + degreeMap);
 
-            degreeMap.keySet().stream().filter(i->i>0)
-                    .forEach(dc->{
-                        degreeMap.get(dc).stream().forEach(i->{
-                            citiesMap.get(i).getNeighbours().stream().forEach(m->{
-                                if (!toBeConnected.contains(m) && !toBeConnected.contains(citiesMap.get(i))){
+            degreeMap.keySet().stream().filter(i -> i > 0)
+                    .forEach(dc -> {
+                        degreeMap.get(dc).stream().forEach(i -> {
+                            citiesMap.get(i).getNeighbours().stream().forEach(m -> {
+                                if (!toBeConnected.contains(m) && !toBeConnected.contains(citiesMap.get(i))) {
                                     toBeConnected.add(m);
                                     toBeConnected.add(citiesMap.get(i));
                                     mergeMap.put(m.getId(), i);
@@ -75,34 +90,19 @@ public class Application {
                         });
                     });
 
-            System.out.println("MergeMap= " + mergeMap);
-            mergeMap.forEach((k,v) -> {
+            //System.out.println("MergeMap= " + mergeMap);
+            mergeMap.forEach((k, v) -> {
                 Node m = citiesMap.get(k);
                 Node p = citiesMap.get(v);
                 m.getMergedNodes().add(p.getId());
                 m.getNeighbours().remove(p);
-                p.getNeighbours().stream().filter(n -> n.getId() != m.getId()).forEach(n->{
+                p.getNeighbours().stream().filter(n -> n.getId() != m.getId()).forEach(n -> {
                     m.addNeighbour(n);
                     n.addNeighbour(m);
                 });
-                p.getMergedNodes().stream().forEach(i->m.getMergedNodes().add(i));
+                p.getMergedNodes().forEach(i -> m.getMergedNodes().add(i));
                 p.clearAllNeighbours();
             });
-/*
-            mergeMap.forEach((k,v) -> {
-                Node m = citiesMap.get(k);
-                Node p = citiesMap.get(v);
-                //m.merge(p);
-
-                p.getNeighbours().stream().forEach(n->m.addNeighbour(n));
-                p.getMergedNodes().stream().forEach(i->m.getMergedNodes().add(i));
-                p.clearAllNeighbours();
-                m.getMergedNodes().add(p.getId());
-                m.getNeighbours().remove(p);
-
-            });
-
- */
 
             toBeConnected.stream()
                     .filter(node -> !mergeMap.containsKey(node.getId()))
@@ -111,10 +111,8 @@ public class Application {
             mergeMap.clear();
             toBeConnected.clear();
             ++step;
-            System.out.println("CitiesMap= " +  citiesMap);
+            //System.out.println("CitiesMap= " + citiesMap);
         }
         return step;
-
-
     }
 }
