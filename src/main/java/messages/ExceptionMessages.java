81304586c73e940227f3f6f3dce3e72907c74dfa
Emre Sercan Aslan <emre55aslan@gmail.com>
2019-09-22 11:18:16 +0300

    Added final to messages and organized unused imports at apptest

--- src/main/java/messages/ExceptionMessages.java
+++ src/main/java/messages/ExceptionMessages.java
@@ -1,11 +1,11 @@
 package messages;
 
 public final class ExceptionMessages {
-    public static String NUMBER_FORMAT_ERR = "Number Format Error!";
-    public static String INPUT_ERROR_MESSAGE = "Input Mismatch Error!";
-    public static String NO_SOLVER_DEFINED = "Solution of this problem is not defined!";
-    public static String NUM_OF_CITIES_RANGE_ERR = "Number of cities must be grater than 1 and less than 601!";
-    public static String NUM_OF_ROADS_RANGE_ERR = "Number of roads must be equal to (number of cities - 1)!";
-    public static String ROAD_PATH_OUT_OF_RANGE_ERR = "Road path out of the range!";
-    public static String GRAPH_IS_NOT_ACYCLIC = "Graph is not acyclic!";
+    public static final String NUMBER_FORMAT_ERR = "Number Format Error!";
+    public static final String INPUT_ERROR_MESSAGE = "Input Mismatch Error!";
+    public static final String NO_SOLVER_DEFINED = "Solution of this problem is not defined!";
+    public static final String NUM_OF_CITIES_RANGE_ERR = "Number of cities must be grater than 1 and less than 601!";
+    public static final String NUM_OF_ROADS_RANGE_ERR = "Number of roads must be equal to (number of cities - 1)!";
+    public static final String ROAD_PATH_OUT_OF_RANGE_ERR = "Road path out of the range!";
+    public static final String GRAPH_IS_NOT_ACYCLIC = "Graph is not acyclic!";
 }
--- src/test/java/ApplicationTest.java
+++ src/test/java/ApplicationTest.java
@@ -1,6 +1,4 @@
 import messages.ExceptionMessages;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
