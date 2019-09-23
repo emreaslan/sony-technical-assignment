236b81efa546e371297de07514fdda6afc3e116e
Emre Aslan <emre55aslan@gmail.com>
2019-09-23 09:17:13 +0300

    Closes #25

--- src/main/java/messages/ExceptionMessages.java
+++ src/main/java/messages/ExceptionMessages.java
@@ -4,10 +4,10 @@ public final class ExceptionMessages {
     public static final String NUMBER_FORMAT_ERR = "Number Format Error!";
     public static final String INPUT_ERROR_MESSAGE = "Input Mismatch Error!";
     public static final String NO_SOLVER_DEFINED = "Solution of this problem is not defined!";
-    public static final String NUM_OF_CITIES_RANGE_ERR = "Number of cities must be grater than 1 and less than 601!";
+    public static final String NUM_OF_CITIES_RANGE_ERR = "Number of cities range error!";
     public static final String NUM_OF_ROADS_RANGE_ERR = "Number of roads must be equal to (number of cities - 1)!";
     public static final String ROAD_PATH_OUT_OF_RANGE_ERR = "Road path out of the range!";
     public static final String GRAPH_IS_NOT_ACYCLIC = "Graph is not acyclic!";
-    public static final String NUM_OF_PROBLEMS_RANGE_ERR = "Number of problems must be grater than 1 and less than 1001!";
+    public static final String NUM_OF_PROBLEMS_RANGE_ERR = "Number of problems range error!";
     public static final String GRAPH_PROBLEM_DEFINITION_ERR = "Graph problem definition error!";
 }
